d238aa2c9148e94173aa349e48548ab4d058fc37
Pradeep Gupta <pradeep.gupta@grocermax.com>
2015-12-15 22:28:21 +0530

    - deleted all files from develop branch

--- scripts/apps/filter/product.js
+++
@@ -1,8 +0,0 @@
-define(['app'], function(app){
-	app.filter('sampleFilter', function($rootScope) {
-		return function( items, types) {
-		    var output = [];
-		    return output;		    
-		};
-	});
-});
