3d661162b9d329208bffec8df1a2c47e969e2e14
moh mustakeem <mohd.mustakeem@grocermax.com>
2016-04-04 19:03:27 +0530

    static location popup

--- scripts/apps/controllers/category.js
+++ scripts/apps/controllers/category.js
@@ -30,21 +30,21 @@ define(['app'], function(app) {
             $scope.categoryName = "";
             $scope.bannerList = null;
             $scope.quoteId = angular.isDefined(utility.getJStorageKey("quoteId")) && utility.getJStorageKey("quoteId") ? utility.getJStorageKey("quoteId") : null;
-            $scope.cityList = null;
-            /*$scope.cityList = [{
+            //$scope.cityList = null;
+            $scope.cityList = [{
 				api_url: 		"api/",
 				city_name:		"Gurgaon",
 				default_name:	"Haryana",
 				id:				"1",
 				region_id:		"487",
 				storeid:		"1"
-			}];*/
+			}];
 			$scope.cityLocation = {};
             $scope.categoryImageUrl = null;
             $scope.myInterval = 5000;
             $scope.noWrapSlides = false;
             $scope.carouselIndex = 0;
-			$scope.modalHide = false;
+			//$scope.modalHide = false;
 			
             $scope.pageRoute = {
                 "faq": false,
@@ -482,8 +482,8 @@ define(['app'], function(app) {
                 if(angular.isDefined(utility.getJStorageKey("cityList"))
                     && utility.getJStorageKey("cityList")) {
                     $scope.cityList = utility.getJStorageKey("cityList");
-                    openCitySelectionModal();
-                } else {                
+                    //openCitySelectionModal();
+                } /*else {                
                     utility.getCityList()
                         .then(function(data){
                             $scope.cityList = data.location;
@@ -494,12 +494,12 @@ define(['app'], function(app) {
                             });                            
                             openCitySelectionModal();
                         });
-                }
+                }*/
             };
 
-            $scope.editLocation = function() {
+            /*$scope.editLocation = function() {
                 getCityList();
-            };
+            };*/
 
             hideCitySelectionModal = function() {
                 $('#myModal').modal('hide');
@@ -509,9 +509,9 @@ define(['app'], function(app) {
                 var city = location.city_name.toLowerCase(),
                     cityId = location.id;
 
-                angular.forEach($scope.cityLocation, function(value, key){
+                /*angular.forEach($scope.cityLocation, function(value, key){
                     $scope.cityLocation[key] = false;
-                });
+                });*/
                 $scope.cityLocation[city] = true;
                 $scope.selectedCity = city;
                 utility.setJStorageKey("selectedCity", city, 1);
